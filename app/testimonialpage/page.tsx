0c9eacb2944bf651ee7fe543aead5dc28abeea91
YagaAndrew <andrewyaganovianta@gmail.com>
2026-08-05 09:55:14 +0700

    feat: Add testimonialpage

---
+++ app/testimonialpage/page.tsx
@@ -0,0 +1,8 @@
+import TestimonialSection from "@/components/pages/testimonialpage/TestimonialSection";
+import React from "react";
+
+const page = () => {
+  return <TestimonialSection />;
+};
+
+export default page;
